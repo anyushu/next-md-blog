a5ba994a938d8e06455a822f068b446162ef69f7
anyushu <suzuki@lamp.jp>
2022-04-07 11:45:33 +0900

    add pagenavi component

--- src/utils/blog-helper.ts
+++ src/utils/blog-helper.ts
@@ -1,3 +1,11 @@
+/**
+ * display posts number
+ */
+export const PER_PAGE = 2
+
+/**
+ * get ogp image url
+ */
 export const ogpImageUrl = () => {
   return process.env.NEXT_PUBLIC_SITE_URL + '/images/ogp.png'
 }
