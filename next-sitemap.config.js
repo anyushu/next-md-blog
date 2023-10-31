88787eb9c1cc3f255458750656b54bcfc03395d3
anyushu <anyushu0526@gmail.com>
2023-10-31 17:41:05 +0900

    update next 14

--- next-sitemap.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-const config = {
-  siteUrl: process.env.NEXT_PUBLIC_SITE_URL || 'https://example.com',
-  generateRobotsTxt: true,
-}
-
-module.exports = config
--- next.config.js
+++ next.config.js
@@ -1,11 +1,4 @@
 /** @type {import('next').NextConfig} */
+const nextConfig = {}
 
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
-})
-
-const config = withBundleAnalyzer({
-  reactStrictMode: true,
-})
-
-module.exports = config
+module.exports = nextConfig
