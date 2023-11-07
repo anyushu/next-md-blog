03805317f2f3e641f974848ed3eedc4b66f3102d
anyushu <anyushu0526@gmail.com>
2023-11-07 13:29:02 +0900

    contentlayerのインストール

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,18 @@
+import { allPosts } from 'contentlayer/generated'
+import { compareDesc } from 'date-fns'
+
 const Home = () => {
-  return <h1>home</h1>
+  const posts = allPosts.sort((a: { date: string | number | Date }, b: { date: string | number | Date }) =>
+    compareDesc(new Date(a.date), new Date(b.date))
+  )
+  return (
+    <div>
+      <ul>
+        {posts.map((post) => (
+          <li key={post._id}>{post.title}</li>
+        ))}
+      </ul>
+    </div>
+  )
 }
 export default Home
