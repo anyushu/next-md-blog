1824ec2c558403305175f8efad680f4629701193
anyushu <anyushu0526@gmail.com>
2023-11-07 15:06:27 +0900

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import '@/styles/globals.css'
 import type { Metadata } from 'next'
 
 export const metadata: Metadata = {
-  title: process.env.SITE_NAME || 'OrderConnect Template',
+  title: process.env.SITE_NAME || 'anyushu',
   description: ''
 }
 
