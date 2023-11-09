23d566c24809a630d53de40f90cec1f3b5124d63
anyushu <anyushu0526@gmail.com>
2023-11-09 14:35:59 +0900

    font変更

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,22 @@
 import Footer from '@/components/footer'
 import { ThemeProvider } from '@/components/function/theme-provider'
 import Header from '@/components/header'
+import { cn } from '@/lib/utils'
 import '@/styles/globals.css'
 import type { Metadata } from 'next'
+import { Noto_Sans_JP as FontSans } from 'next/font/google'
 
 export const metadata: Metadata = {
   title: process.env.SITE_NAME || 'anyushu',
   description: 'anyushuの個人ブログ'
 }
 
+const fontSans = FontSans({ subsets: ['latin'], display: 'swap' })
+
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <html lang="ja" suppressHydrationWarning>
-      <body className="relative flex min-h-screen flex-col">
+      <body className={cn('relative flex min-h-screen flex-col', fontSans.className)}>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
           <Header />
           <main className="container flex-1 py-6">{children}</main>
