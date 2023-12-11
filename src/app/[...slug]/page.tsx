4d45230b296ef1d955f79964fe60b56c96bf1bb5
anyushu <anyushu0526@gmail.com>
2023-12-11 18:43:08 +0900

    robots,authors,canonicalの追加

--- src/app/[...slug]/page.tsx
+++ src/app/[...slug]/page.tsx
@@ -1,10 +1,11 @@
 import { allPages } from 'contentlayer/generated'
 import parse from 'html-react-parser'
+import type { Metadata } from 'next'
 import { notFound } from 'next/navigation'
 
 export const generateStaticParams = async () => allPages.map((page) => ({ slug: page.slugAsParams.split('/') }))
 
-export const generateMetadata = ({ params }: { params: { slug: string[] } }) => {
+export const generateMetadata = ({ params }: { params: { slug: string[] } }): Metadata => {
   const slug = params?.slug?.join('/')
   const page = allPages.find((page) => page.slugAsParams === slug)
 
@@ -12,7 +13,13 @@ export const generateMetadata = ({ params }: { params: { slug: string[] } }) => 
     notFound()
   }
 
-  return { title: page.title + ' - anyushu', description: page.description }
+  return {
+    title: page.title + ' - anyushu',
+    description: page.description,
+    alternates: {
+      canonical: `${process.env.SITE_URL}posts/${slug}`
+    }
+  }
 }
 
 const Page = ({ params }: { params: { slug: string[] } }) => {
