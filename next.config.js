14fe89d1a61eb6cc2118afa106a107de9fec6422
anyushu <anyushu0526@gmail.com>
2024-02-14 12:51:08 +0900

    fix config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,6 @@
+import { withContentlayer } from 'next-contentlayer'
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = withContentlayer({})
 
 module.exports = nextConfig
